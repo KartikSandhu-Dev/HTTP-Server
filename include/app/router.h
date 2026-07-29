6f3ff33aba7e403cecfe340eb8f94999e1c9cf79
KartikSandhu-Dev <kartiksandhu888@gmail.com>
2026-07-29 20:20:24 +0530

    routehandler fix

--- include/app/router.h
+++ include/app/router.h
@@ -4,13 +4,12 @@
 #include "http/request.h"
 #include "http/response.h"
 
-typedef void (*RouteHandler)(HttpRequest *request, HttpResponse *response, const char *data);
+typedef void (*RouteHandler)(HttpRequest *request, HttpResponse *response);
 
 typedef struct Route {
 	const char *method;
 	const char *path;
 	RouteHandler route_handler;
-	const char *data;
 } Route;
 
 void router_handle(HttpRequest *request, HttpResponse *response);
